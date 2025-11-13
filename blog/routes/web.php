70759335f900781bab26232631ccefc8de81cb6f
= <=>
2025-11-13 11:12:53 +0100

    save

--- blog/routes/web.php
+++ blog/routes/web.php
@@ -1,7 +1,15 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PageController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route test
+Route::get('/ping', fn() => 'pong');
+
+// Routes nommées avec contrôleur
+Route::get('/', [PageController::class, 'home'])->name('home');
+Route::get('/a-propos', [PageController::class, 'about'])->name('about');
+
+// Mini-routes articles (mockées pour l’instant)
+Route::get('/articles', [PageController::class, 'articles'])->name('articles.index');
+Route::get('/articles/{slug}', [PageController::class, 'show'])->name('articles.show');
